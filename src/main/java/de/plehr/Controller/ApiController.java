0008e035b156935a57c10f66359c32b612264cf9
Pascal Lehr <spam@plehr.de>
2022-02-28 01:55:32 +0200

    extend API to get DataEntry's

--- src/main/java/de/plehr/Controller/ApiController.java
+++ src/main/java/de/plehr/Controller/ApiController.java
@@ -1,20 +1,32 @@
 package de.plehr.Controller;
 
+import java.util.List;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.plehr.MqttCommunicator;
+import de.plehr.Repository.EntryRepository;
+import de.plehr.Model.DataEntry;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
+    @Autowired
+    EntryRepository entryRepository;
+
     @RequestMapping("/values")
-    String getValues() throws MqttPersistenceException, MqttException {
-       // new MqttCommunicator().sendMessage("helloooo", "Pascal");
-        return "hello";
+    public List<DataEntry> getValues() {
+        return entryRepository.findAll();
     }
-    
+
+    @RequestMapping("/set")
+    String setCommand() throws MqttPersistenceException, MqttException {
+        new MqttCommunicator().sendMessage("helloooo", "Pascal");
+        return "";
+    }
+
 }
