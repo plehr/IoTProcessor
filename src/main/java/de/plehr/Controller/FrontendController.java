5f35b9a0ed2277bb953fcb49050a92ed6c9c82e9
Pascal Lehr <spam@plehr.de>
2022-02-28 21:39:18 +0200

    value table for fontend

--- src/main/java/de/plehr/Controller/FrontendController.java
+++ src/main/java/de/plehr/Controller/FrontendController.java
@@ -3,14 +3,26 @@ package de.plehr.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import de.plehr.Repository.EntryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class FrontendController {
 
+    @Autowired
+    private EntryRepository entryRepository;
+
     @GetMapping("/")
     String firstPage(Model m) {
         m.addAttribute("hello", "Helloo World!");
         return "firstPage";
+    }
+
+    @GetMapping("/values")
+    String getValues(Model m) {
+        m.addAttribute("valuelist", entryRepository.findAll());
+        return "values";
     }
 }
