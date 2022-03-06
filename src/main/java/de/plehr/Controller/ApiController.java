bfa8cae58e4ef0d0fa4f65b31a09cb43e3147d70
Pascal Lehr <spam@plehr.de>
2022-03-06 23:36:36 +0200

    add return channel

--- src/main/java/de/plehr/Controller/ApiController.java
+++ src/main/java/de/plehr/Controller/ApiController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.plehr.Repository.EntryRepository;
 import de.plehr.Model.DataEntry;
-//import de.plehr.Mqtt.MqttCommunicator;
+import de.plehr.Mqtt.MqttCommunicator;
 
 @RestController
 @RequestMapping("/api")
@@ -18,15 +20,14 @@ public class ApiController {
     @Autowired
     EntryRepository entryRepository;
 
-    @RequestMapping("/values")
+    @RequestMapping(value="/values", method=RequestMethod.GET)
     public List<DataEntry> getValues() {
         return entryRepository.findAll();
     }
 
-    @RequestMapping("/set")
-    String setCommand() throws MqttPersistenceException, MqttException {
-      //  new MqttCommunicator().sendMessage("helloooo", "Pascal");
+    @RequestMapping(value="/source/{source}/interval/{interval}", method = RequestMethod.POST)
+    String setInterval(@PathVariable("source") String source,@PathVariable("interval") int interval) throws MqttPersistenceException, MqttException {
+        new MqttCommunicator().sendMessage(source+"/setInterval", interval+"");
         return "";
     }
-
 }
